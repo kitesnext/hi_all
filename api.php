0f9141efbf8599d8e5d8879fff323234d59a85ff
kitesnext <32243373+kitesnext@users.noreply.github.com>
2017-10-04 14:59:11 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,13 +1,17 @@
 <?php
+
 $status = 'OK';
 $response = null;
 $error = 0;
 $company = null;
 $requeststatus = 0;
 $answer = "No Companies Passed from BaseBid check" ;
+$winnercompany = null;
+$winner = null;
 
 $budget = 0;
 $bid = 0;
+$items = array(); //   Response JSON
 
   $servername = "localhost";
   $username = "cp62865_st";
@@ -44,44 +48,62 @@ if(!isset($_GET['BaseBid'])){ // если не получили параметр
    die("Connection failed: " . mysqli_connect_error());
   }
 
-$sql=   "SELECT companies.Company, country.Country, category.Category, companies.Budget, companies.Bid
- FROM companies JOIN country ON companies.CompanyID=country.CompanyID 
- JOIN category ON country.CompanyID=category.CompanyID
- WHERE companies.Budget>=".$basebid." AND companies.Bid>=".$basebid." AND country.Country='".$country."' AND category.Category='".$category."'
- ORDER BY companies.Bid DESC"; 
-$result_set = $connect->query($sql);    
+//--------------- Check  Targeting”
 
-if (!$result_set) {
-    echo "select_error";
-    exit;
-}    
-     $items = array(); // Массив  Response
-   //  while (($row = $result_set->fetch_assoc()) != false) $items[] = $row; // 
-     $items = $result_set->fetch_assoc();
+$targeting = "SELECT companies.Company, country.Country, category.Category, companies.Bid, companies.Budget
+ FROM companies
+ LEFT JOIN country ON country.CompanyID = companies.CompanyID AND country.Country='".$country."'
+ LEFT JOIN category ON category.CompanyID = country.CompanyID AND category.Category='".$category."'
+ ORDER BY companies.Bid DESC";
+
  
+$targeting_set = $connect->query($targeting);
 
-if ($items[Company] != null) { 
-$company = $items[Company];
-$budget = $items[Budget];
-$bid = $items[Bid];
-$requeststatus = 1; 
-$answer = "Winner is ".$company;  
+while (($row1 = $targeting_set->fetch_assoc()) != false) {
+ 
+   $requeststatus = 0;
+   
+  if ($row1[Category] != null && $row1[Country] != null) $targeting_pass = 'Target Passed';
+  else $targeting_pass = 'Target Failed';
+  
+  if ($row1[Bid] >= $basebid) $bid_pass = 'Bid Passed';
+  else $bid_pass = 'Bid Failed';
+  
+  if ($row1[Budget] >= $basebid) $budget_pass = 'Budget Passed';
+  else $budget_pass = 'Budget Failed';
+   
+  //------- Choising of winner 
+  if ($winnercompany == null && $row1[Category] != null && $row1[Country] != null && $row1[Bid] >= $basebid && $row1[Budget] >= $basebid) {
+    
+    $winnercompany = $row1[Company];
+    $winnerbudget = $row1[Budget];
+    $winnerbid = $row1[Bid];
+    $requeststatus = 1; 
+    $answer = "Winner is ".$winnercompany;
+    $items = $row1; 
+    
+   }
+  
+  
+//-----------------Insert all companies in Log  
+
+   
+$insertlog = "INSERT INTO logfile1 (ReqCountry, ReqCategory, ReqBid, ReqStatus, Company, Budget, Bid, TargCheck, BudgetCheck, BidCheck)
+  VALUES ('".$country."', '".$category."', ".$basebid.", ".$requeststatus.", '".$row1[Company]."', ".$row1[Budget].", ".$row1[Bid].", '".$targeting_pass."', '".$budget_pass."', '".$bid_pass."')";
+$insert_set = $connect->query($insertlog);
+ 
+  if (!$insert_set) {
+      $status = 'ERROR logfile';
+      $error = 2;
+  } 
+  
 }
 
- //--------2-- Inset Log DB   
-$insert = "INSERT INTO log (Country, Category, BaseBid, StatusRequest, Company, Budget, Bid)
- VALUES ('".$country."', '".$category."', ".$basebid.", ".$requeststatus.", '".$company."', ".$budget.", ".$bid.")";
-$insert_set = $connect->query($insert);
-//$insert_set = $connect->query( "INSERT INTO log (Country) VALUES ('US')");  
-if (!$insert_set) {
+    //-------- Changing New Budget DB
 
-    $status = 'ERROR log';
-    $error = 2;
-}   
+if  ($winnercompany != null) {      // if winner 
 
-if  ($requeststatus = 1) {      // if OK
-    //------1-- Changing DB
-    $update="UPDATE companies SET Budget = Budget - ".$basebid." WHERE Company = '".$company."'";
+    $update="UPDATE companies SET Budget = Budget - ".$basebid." WHERE Company = '".$winnercompany."'";
     $update_set = $connect->query($update);  
     if (!$update_set) {
         $status = 'ERROR Update Budget';
@@ -89,36 +111,24 @@ if  ($requeststatus = 1) {      // if OK
     }      
 }
 
-
-//------------
-/*
-if(!isset($_GET['email'])){ // если не получили параметр
-    $status = 'ERROR';
-    $error = 1;
-}else{
-    $email = $_GET['email'];
-    if(preg_match("|^[-0-9a-z_\.]+@[-0-9a-z_^\.]+\.[a-z]{2,6}$|i", $email)){ // проверяем корректрость e-mail
-        $response = true;
-    }else{
-        $response = false;
-    }
-}
-*/
 // массив для ответа
 $result = array(
     'status' => $status,
     
     'error' => $error,
 );
-//$company = $items[Company];
-echo json_encode($company);
-echo json_encode($country);
-echo json_encode($category);
-echo json_encode($basebid);
-echo json_encode($bid);
-echo json_encode($budget);
-echo json_encode($requeststatus);
-echo json_encode('<Br>'.$answer);
-echo json_encode('<Br>'.$items);
-echo json_encode($result); // ответ в формате json
+
+//echo '<Br>---------------- WINNER VIEW------------- <br>';
+echo json_encode($answer);
+echo '<Br>---------------- JSON ANSWER------------- <br>';
+echo json_encode($items);
+//echo '<Br>---------------- ERROR VIEW------------- <br>';
+//echo json_encode($result); // ответ в формате json
+
+echo '<Br>---------------- LOG VIEW------------- <br>';
+
+$result_log = $connect->query("SELECT * FROM logfile1 ORDER BY ActID DESC");
+while (($row = $result_log->fetch_assoc()) != false) echo json_encode($row).'<br />';
+mysql_close();
+
 ?>
